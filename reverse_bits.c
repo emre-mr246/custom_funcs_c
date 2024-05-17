324b7899d7453087e8204d1f3b191da40e179a33
Emre Gül <144537902+emre-mr246@users.noreply.github.com>
2024-05-18 00:12:58 +0300

    Update reverse_bits.c

--- reverse_bits.c
+++ reverse_bits.c
@@ -1,3 +1,9 @@
+/**
+ * @brief Reverses the bits of an unsigned char.
+ *
+ * @param octet The unsigned char to reverse the bits of.
+ * @return The unsigned char with its bits reversed.
+ */
 unsigned char	reverse_bits(unsigned char octet)
 {
 	int		i;
