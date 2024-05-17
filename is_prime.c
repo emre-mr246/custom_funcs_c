f67f7fd243739d69db94f8dd7e60e03aee4eb7f0
Emre Gül <144537902+emre-mr246@users.noreply.github.com>
2024-05-18 00:07:42 +0300

    Update is_prime.c

--- is_prime.c
+++ is_prime.c
@@ -10,23 +10,23 @@
  * which are already checked earlier. If a divisor is found, the number is not prime
  * and the function returns 0; otherwise, it returns 1 indicating primality.
  */
-int is_prime(int nbr)
+int	is_prime(int nbr)
 {
-	int i;
+	int	i;
 
 	i = 0;
-    if (nbr <= 1)
-        return (0);
-    if (nbr <= 3)
-        return (1);
-    if ((nbr % 2 == 0) || (nbr % 3 == 0))
-        return (0);
+	if (nbr <= 1)
+		return (0);
+	if (nbr <= 3)
+		return (1);
+	if ((nbr % 2 == 0) || (nbr % 3 == 0))
+		return (0);
 	i = 5;
-    while (i * i <= nbr) 
+	while (i * i <= nbr)
 	{
-        if ((nbr % i == 0) || (nbr % (i + 2) == 0))
-            return (0);
+		if ((nbr % i == 0) || (nbr % (i + 2) == 0))
+			return (0);
 		i += 6;
-    }
-    return (1);
+	}
+	return (1);
 }
